d8293b8dce3c7429a919e7c8991516f0fd0c139f
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2025-11-28 15:51:51 +0530

    webm video added

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Poster from "@/public/assets/images/poster.png";
 
 export default function HeroSection() {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -17,17 +18,31 @@ export default function HeroSection() {
     };
   }, []);
   console.log("scrollPosition :", scrollPosition);
-
+  const webm = "/assets/videos/ONE_FAM_MEDIA_HUB.webm";
   return (
     <div className="relative top-0">
       <div className="w-screen h-screen overflow-hidden">
-        <video
+        {/* <video
           src="/assets/videos/ONE_FAM_MEDIA_HUB.mp4"
           autoPlay
           muted
           playsInline
+          preload="metadata"
+          poster={Poster}
           className="w-full h-full object-cover"
-        />
+        /> */}
+        <video
+          autoPlay
+          muted
+          playsInline
+          preload="metadata"
+          poster={Poster}
+          className="w-full h-full object-cover"
+        >
+          {webm && <source src={webm} type="video/webm" />}
+          <source src="/assets/videos/ONE_FAM_MEDIA_HUB.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
